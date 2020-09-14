29b71247502237575581bf4a5c8a4a39d44d2775
vijay-busy <62492984+vijay-busy@users.noreply.github.com>
2020-09-14 09:41:26 +0530

    Update 2 script.js
    added check button

--- todo/script.js
+++ todo/script.js
@@ -7,26 +7,41 @@ function add(){
     else{
         var p=document.createElement('p');
         var input=document.createElement('input');
-        var button=document.createElement('button');
-
+        var button1=document.createElement('button');
+        var button2=document.createElement('button');
+        
         var t=document.createTextNode(txt);
         input.appendChild(t);
         input.setAttribute('id','list'+z);
         input.setAttribute('value',txt);
-
+    
         var x=document.createTextNode('x');
-        button.appendChild(x);
-        button.setAttribute('id','btn'+z)
+        button1.appendChild(x);
+        button1.setAttribute('id','btn'+z);
+        var v=document.createTextNode('√');
+        button2.appendChild(v);
+        button2.setAttribute('id','btn2'+z);
+        
         p.appendChild(input);
-        p.appendChild(button);
+        p.appendChild(button1);
+        p.appendChild(button2);
+        
+        button2.style.backgroundColor='green';
+        
         document.getElementById('myList').appendChild(p);
         document.getElementById('inputField').value='';
 
-        button.addEventListener('click',function exit(){
+        button1.addEventListener('click',function exit(){
             var a=input.id;
-            var b=button.id;
+            var b=button1.id;
+            var c=button2.id;
             document.getElementById(a).remove();
             document.getElementById(b).remove();
+            document.getElementById(c).remove();
+        });
+        button2.addEventListener('click',function check(){
+            var d=input.id;
+            document.getElementById(d).style.backgroundColor='lightGreen';
         });
         z++;
     }
