bfa0af51d1f1ee0e02a37246d385e94b41f5f813
vijay-busy <62492984+vijay-busy@users.noreply.github.com>
2020-09-14 09:07:22 +0530

    Update script.js

--- todo/script.js
+++ todo/script.js
@@ -1,52 +1,33 @@
-// var z=0;
-// function add(){
-//     var li=document.createElement('li');
-//     var span=document.createElement('span');
-//     var button=document.createElement('button');
-
-//     var txt=document.getElementById('inputField').value;
-//     var t=document.createTextNode(txt);
-//     li.appendChild(t);
-//     li.setAttribute('id','list'+z);
-//     z++;
-
-//     var x=document.createTextNode('x');
-//     button.appendChild(x);
-//     li.appendChild(span);
-//     span.appendChild(button);
-//     document.getElementById('myList').appendChild(li);
-//     document.getElementById('inputField').value=' ';
-
-//     button.addEventListener('click',function exit(){
-//         var y=li.id;
-//         document.getElementById(y).remove();
-//      });
-// }
 var z=0;
 function add(){
-    var p=document.createElement('p');
-    var input=document.createElement('input');
-    var button=document.createElement('button');
-
     var txt=document.getElementById('inputField').value;
-    var t=document.createTextNode(txt);
-    input.appendChild(t);
-    input.setAttribute('id','list'+z);
-    input.setAttribute('value',txt);
+    if(txt===""){
+        alert("Enter something");
+    }
+    else{
+        var p=document.createElement('p');
+        var input=document.createElement('input');
+        var button=document.createElement('button');
 
-    var x=document.createTextNode('x');
-    button.appendChild(x);
-    button.setAttribute('id','btn'+z)
-    p.appendChild(input);
-    p.appendChild(button);
-    document.getElementById('myList').appendChild(p);
-    document.getElementById('inputField').value=' ';
+        var t=document.createTextNode(txt);
+        input.appendChild(t);
+        input.setAttribute('id','list'+z);
+        input.setAttribute('value',txt);
 
-    button.addEventListener('click',function exit(){
-        var a=input.id;
-        var b=button.id;
-        document.getElementById(a).remove();
-        document.getElementById(b).remove();
-    });
-    z++;
+        var x=document.createTextNode('x');
+        button.appendChild(x);
+        button.setAttribute('id','btn'+z)
+        p.appendChild(input);
+        p.appendChild(button);
+        document.getElementById('myList').appendChild(p);
+        document.getElementById('inputField').value='';
+
+        button.addEventListener('click',function exit(){
+            var a=input.id;
+            var b=button.id;
+            document.getElementById(a).remove();
+            document.getElementById(b).remove();
+        });
+        z++;
+    }
 }
